a363a290edf3c78e91f819e8f13c12ecb60180e1
SNEHA L <84729040+Sneha-15-2001@users.noreply.github.com>
2023-07-26 12:39:54 +0530

    Add files via upload

---
+++ Sneha L/Class Work/Attendance.java
@@ -0,0 +1,20 @@
+public class Attendance{
+	void markAttendance()
+		{
+			System.out.println("MASS BUNK");
+		}
+		void markAttendance(String... s)
+		{
+			System.out.println("No. of Students present is: "+s.length);
+			System.out.println("Name of the Students Present:");
+			for(String i:s){
+
+				System.out.println(i);
+			}
+		}
+	public static void main(String args[])
+	{
+		Attendance present=new Attendance();
+		present.markAttendance("Tom","Sam","Jhon");
+	}
+}
