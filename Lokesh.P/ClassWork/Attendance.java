499bdb4d198c2d0d4dead3f3e2a4d12fd8f1b2f6
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2023-07-26 12:23:04 +0530

    Add files via upload

---
+++ Lokesh.P/ClassWork/Attendance.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Attendance {
+        void MarkAttendance(){
+            System.out.println("Mass bunk");
+        }
+        void MarkAttendance(String...s){
+            System.out.println(s.length+" Students are present in the class");
+            for(String name:s){
+                System.out.println(name);
+            }
+        }
+    public static void main(String[] args) {
+        Attendance m=new Attendance();
+        m.MarkAttendance("Lokesh","Jerish","Arun Kumar","Poornesh","Jeeva");
+
+        }
+
+
+
+}
